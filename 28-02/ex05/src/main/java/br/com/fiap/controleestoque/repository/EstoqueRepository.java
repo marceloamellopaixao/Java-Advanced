e23a83c7c90d45ed6787a79c1c1b544639d8eb3f
marceloamellopaixao <marceloamellopaixao@gmail.com>
2024-03-13 21:07:18 -0300

    Revert "update"
    This reverts commit 5cc87c121e07361abba279bb96e88136ec14874f.

--- 28-02/ex05/src/main/java/br/com/fiap/controleestoque/repository/EstoqueRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.controleestoque.repository;
-
-import br.com.fiap.controleestoque.model.Estoque;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstoqueRepository extends JpaRepository<Estoque, Long> {
-}
