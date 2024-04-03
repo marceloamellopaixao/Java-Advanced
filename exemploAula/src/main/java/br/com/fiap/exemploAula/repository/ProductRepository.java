602cd37f6527c928ba8b30e11d3e2dfdb0060591
marceloamellopaixao <marceloamellopaixao@gmail.com>
2024-04-03 20:33:25 -0300

    att

--- exemploAula/src/main/java/br/com/fiap/exemploAula/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.exemploAula.repository;
-
-import br.com.fiap.exemploAula.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
